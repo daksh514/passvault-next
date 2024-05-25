5594d13b71c0aa0e7d6d790b22be28430003c576
Daksh MAngal <mangalpradeepkumar@gmail.com>
2024-05-25 14:21:29 +0530

    another secret fixing

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -9,5 +9,6 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
     ],
+    secret :process.env.AUTH_SECRET as string,
     adapter: MongoDBAdapter(clientPromise),
 } as AuthOptions
